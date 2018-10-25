b57692dbe8ff93e6b7a732ac5776bb857abcfbcf
Nick Hirakawa <nickhirakawa@gmail.com>
2018-10-25 18:19:01 -0400

    Add vector class

---
+++ src/main/java/com/github/nhirakawa/ray/tracing/geometry/Vector3.java
@@ -0,0 +1,59 @@
+package com.github.nhirakawa.ray.tracing.geometry;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public class Vector3 extends Vector3D {
+
+  public Vector3(double x, double y, double z) {
+    super(x, y, z);
+  }
+
+  public double getRed() {
+    return getX();
+  }
+
+  public double getGreen() {
+    return getY();
+  }
+
+  public double getBlue() {
+    return getZ();
+  }
+
+  public Vector3 add(Vector3 vector) {
+    return from(add(vector));
+  }
+
+  public Vector3 multiply(Vector3 vector) {
+    double x = getX() * vector.getX();
+    double y = getY() * vector.getY();
+    double z = getZ() * vector.getZ();
+
+    return new Vector3(x, y, z);
+  }
+
+  public Vector3 divide(Vector3 vector) {
+    double x = getX() / vector.getX();
+    double y = getY() / vector.getY();
+    double z = getZ() / vector.getZ();
+
+    return new Vector3(x, y, z);
+  }
+
+  public Vector3 scalarMultiply(double scalar, Vector3 vector) {
+    return from(vector.scalarMultiply(scalar));
+  }
+
+  public Vector3 scalarDivide(double scalar, Vector3 vector) {
+    return vector.scalarMultiply(1 / scalar, vector);
+  }
+
+  public Vector3 cross(Vector3 vector) {
+    return from(crossProduct(vector));
+  }
+
+  private static Vector3 from(Vector3D vector) {
+    return new Vector3(vector.getX(), vector.getY(), vector.getZ());
+  }
+
+}
