8e711695e556387e923c29d459a775cecbe9f23f
Nick Hirakawa <nickhirakawa+github@gmail.com>
2018-12-09 01:01:21 -0500

    basic metrics (#13)

---
+++ src/main/java/com/github/nhirakawa/hyperbeam/Metrics.java
@@ -0,0 +1,19 @@
+package com.github.nhirakawa.hyperbeam;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+
+public class Metrics {
+
+  private static final MetricRegistry METRIC_REGISTRY = new MetricRegistry();
+
+  private Metrics() {}
+
+  public static Timer getTimer(String name) {
+    return METRIC_REGISTRY.timer(name);
+  }
+
+  public static MetricRegistry instance() {
+    return METRIC_REGISTRY;
+  }
+}
