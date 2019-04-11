985db3a5c5d638b4b8985bf642ed3449f940a39e
Nick Hirakawa <nickhirakawa@gmail.com>
2019-04-11 15:23:50 -0400

    rename

--- hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/AdtTestRunner.java
+++ hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/AdtTestRunner.java
@@ -1,9 +1,5 @@
 package com.github.nhirakawa.hyperbeam;
 
-import com.github.nhirakawa.hyperbeam.shape.Box;
-import com.github.nhirakawa.hyperbeam.shape.ShapeAdtTest;
-import com.github.nhirakawa.hyperbeam.shape.ShapeAdtTests;
-
 public class AdtTestRunner {
 
   public static void main(String... args) {
--- hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/shape/ShapeAdt.java
+++ hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/shape/ShapeAdt.java
@@ -3,7 +3,7 @@ package com.github.nhirakawa.hyperbeam.shape;
 import org.derive4j.Data;
 
 @Data
-public abstract class ShapeAdtTest {
+public abstract class ShapeAdt {
 
   interface Cases<R>{
     R BOX(Box box);
