6df9c2ebf241178d988bddd41d04fe47878cf206
Nick Hirakawa <nickhirakawa@gmail.com>
2019-04-11 15:42:21 -0400

    moving sphere

--- hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/shape/ShapeAdt.java
+++ hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/shape/ShapeAdt.java
@@ -5,10 +5,14 @@ import org.derive4j.Data;
 @Data
 public abstract class ShapeAdt {
 
-  interface Cases<R>{
+  interface Cases<R> {
+
     R BOX(BoxModel box);
     R CONSTANT_MEDIUM(ConstantMediumModel constantMedium);
+    R MOVING_SPHERE(MovingSphereModel movingSphere);
+
   }
 
   public abstract <R> R match(Cases<R> cases);
+
 }
