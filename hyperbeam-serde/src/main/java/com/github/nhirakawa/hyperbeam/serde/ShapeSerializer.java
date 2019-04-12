fcbaa46bca4409ceb6db4b569d871c97fa2572e0
Nick Hirakawa <nickhirakawa@gmail.com>
2019-04-11 22:19:41 -0400

    sketch out serializer

--- hyperbeam-serde/src/main/java/com/github/nhirakawa/hyperbeam/serde/ShapeSerializer.java
+++ hyperbeam-serde/src/main/java/com/github/nhirakawa/hyperbeam/serde/ShapeSerializer.java
@@ -1,11 +1,15 @@
 package com.github.nhirakawa.hyperbeam.serde;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.github.nhirakawa.hyperbeam.shape.BoxModel;
 import com.github.nhirakawa.hyperbeam.shape.ShapeAdt;
+import com.github.nhirakawa.hyperbeam.shape.ShapeAdts;
 
 public class ShapeSerializer extends StdSerializer<ShapeAdt> {
 
@@ -14,7 +18,15 @@ public class ShapeSerializer extends StdSerializer<ShapeAdt> {
   }
 
   @Override
-  public void serialize(ShapeAdt value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+  public void serialize(ShapeAdt value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+    Optional<Consumer<JsonGenerator>> maybeGeneratorConsumer = ShapeAdts.caseOf(value)
+        .BOX(this::forBox)
+        .otherwiseEmpty();
+
+    maybeGeneratorConsumer.ifPresent(consumer -> consumer.accept(generator));
+  }
+
+  private Consumer<JsonGenerator> forBox(BoxModel box) {
     throw new UnsupportedOperationException();
   }
 
