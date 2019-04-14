ae7726571bda751b6386dd7411c57306a4a1fb8a
Nick Hirakawa <nickhirakawa@gmail.com>
2019-04-14 15:45:31 -0400

    full-ish deserialization

--- hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/jackson/ShapeAdtDeserializer.java
+++ hyperbeam-core/src/main/java/com/github/nhirakawa/hyperbeam/jackson/ShapeAdtDeserializer.java
@@ -10,10 +10,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.nhirakawa.hyperbeam.shape.BoundingVolumeHierarchy;
+import com.github.nhirakawa.hyperbeam.shape.Box;
+import com.github.nhirakawa.hyperbeam.shape.ConstantMedium;
+import com.github.nhirakawa.hyperbeam.shape.MovingSphere;
+import com.github.nhirakawa.hyperbeam.shape.ReverseNormals;
 import com.github.nhirakawa.hyperbeam.shape.SceneObjectType;
+import com.github.nhirakawa.hyperbeam.shape.SceneObjectsList;
 import com.github.nhirakawa.hyperbeam.shape.ShapeAdt;
 import com.github.nhirakawa.hyperbeam.shape.ShapeAdts;
 import com.github.nhirakawa.hyperbeam.shape.Sphere;
+import com.github.nhirakawa.hyperbeam.shape.XYRectangle;
+import com.github.nhirakawa.hyperbeam.shape.XZRectangle;
+import com.github.nhirakawa.hyperbeam.shape.YZRectangle;
 
 public class ShapeAdtDeserializer extends StdDeserializer<ShapeAdt> {
 
@@ -30,12 +39,40 @@ public class ShapeAdtDeserializer extends StdDeserializer<ShapeAdt> {
     SceneObjectType sceneObjectType = SceneObjectType.valueOf(objectNode.get("shapeType").asText());
 
     switch(sceneObjectType) {
+      case BOUNDING_VOLUME_HIERARCHY:
+        BoundingVolumeHierarchy boundingVolumeHierarchy = parser.getCodec().treeToValue(objectNode, BoundingVolumeHierarchy.class);
+        return ShapeAdts.BOUNDING_VOLUME_HIERARCHY(boundingVolumeHierarchy);
+      case BOX:
+        Box box = parser.getCodec().treeToValue(objectNode, Box.class);
+        return ShapeAdts.BOX(box);
+      case CONSTANT_MEDIUM:
+        ConstantMedium constantMedium = parser.getCodec().treeToValue(objectNode, ConstantMedium.class);
+        return ShapeAdts.CONSTANT_MEDIUM(constantMedium);
+      case MOVING_SPHERE:
+        MovingSphere movingSphere = parser.getCodec().treeToValue(objectNode, MovingSphere.class);
+        return ShapeAdts.MOVING_SPHERE(movingSphere);
+      case REVERSE_NORMALS:
+        ReverseNormals reverseNormals = parser.getCodec().treeToValue(objectNode, ReverseNormals.class);
+        return ShapeAdts.REVERSE_NORMALS(reverseNormals);
+      case SCENE_OBJECTS_LIST:
+        SceneObjectsList sceneObjectsList = parser.getCodec().treeToValue(objectNode, SceneObjectsList.class);
+        return ShapeAdts.SCENE_OBJECTS_LIST(sceneObjectsList);
       case SPHERE:
         Sphere sphere = parser.getCodec().treeToValue(objectNode, Sphere.class);
         return ShapeAdts.SPHERE(sphere);
-    }
+      case XY_RECTANGLE:
+        XYRectangle xyRectangle = parser.getCodec().treeToValue(objectNode, XYRectangle.class);
+        return ShapeAdts.XY_RECTANGLE(xyRectangle);
+      case XZ_RECTANGLE:
+        XZRectangle xzRectangle = parser.getCodec().treeToValue(objectNode, XZRectangle.class);
+        return ShapeAdts.XZ_RECTANGLE(xzRectangle);
+      case YZ_RECTANGLE:
+        YZRectangle yzRectangle = parser.getCodec().treeToValue(objectNode, YZRectangle.class);
+        return ShapeAdts.YZ_RECTANGLE(yzRectangle);
 
-    throw new UnsupportedOperationException();
+      default: // TODO all scene obejcts need to be a scene object ADT
+        throw new IllegalArgumentException(sceneObjectType + " cannot be deserialized");
+    }
   }
 
 }
